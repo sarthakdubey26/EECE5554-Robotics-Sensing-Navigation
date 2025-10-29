6391751bdcbe3023fc6484ecd0b63b4dc402f4e8
sarthakdubey26 <dubey.sart@northeastern.edu>
2025-10-28 20:48:55 -0400

    Lab 3: Fix package structure for autograder compatibility

---
+++ imu/src/vn_driver/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'vn_driver'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Sarthak Dubey',
+    maintainer_email='dubey.sart@northeastern.edu',
+    description='VectorNav IMU driver',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+            'vn_driver = vn_driver.vn_driver:main',
+        ],
+    },
+)
